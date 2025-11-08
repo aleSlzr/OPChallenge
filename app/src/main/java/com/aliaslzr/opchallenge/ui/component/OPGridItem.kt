1c5928e2c0f88f63fe45872f78597c4d73e3e420
Alejandro Salazar <salazar.cruz.alejandro@gmail.com>
2025-11-08 08:08:27 -0600

    - added a placeholder in artist images

--- app/src/main/java/com/aliaslzr/opchallenge/ui/component/OPGridItem.kt
+++ app/src/main/java/com/aliaslzr/opchallenge/ui/component/OPGridItem.kt
@@ -23,12 +23,15 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
+import com.aliaslzr.opchallenge.R
 
 @Composable
 fun OPGridItem(
@@ -55,7 +58,7 @@ fun OPGridItem(
                 model = imageUrl,
                 contentDescription = name,
                 contentScale = ContentScale.Crop,
-                placeholder = ColorPainter(Color.LightGray),
+                placeholder = painterResource(id = R.drawable.landscape_placeholder),
                 modifier =
                     Modifier
                         .fillMaxSize()
